521edf4b8d799d82e2641d890da6ceb2bea4c2ac
Chris Lynch <Chris.Lynch@sas.com>
2019-09-12 19:58:12 +0000

    (AWSVIYA-119) Test failed durring validate params

--- scripts/validate_parameters.sh
+++ scripts/validate_parameters.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+set -v
+set -x
 EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 EC2_REGION=$(echo ${EC2_AVAIL_ZONE}  | sed "s/[a-z]$//")
 
